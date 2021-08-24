b7c59259e26092fc36e5be53612abe7f2d934d0e
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-08-24 16:00:56 +0530

    Updated the documentation

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -6,11 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  /**
+   * Declaring the title variable in string format (Typescript-way) and assigning value to it.
+   */
   title = 'hello-world';
   imgUrl;
-
+  
+  /**
+    * Method with no return type.
+    * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive. 
+    * Called only once in the lifecycle.
+    */
   ngOnInit(): void {
+    
+    /**
+     * Overriding the above value of title variable and assigning a new value to the same variable.
+     */
     this.title = "Hello from BridgeLabz ";
+    
+    /**
+     * Assigning value to the imgUrl variable to implement the concept of Property Binding.
+     */
     this.imgUrl = "../assets/BL_logo_square_jpg.jpg";
   }
 }
